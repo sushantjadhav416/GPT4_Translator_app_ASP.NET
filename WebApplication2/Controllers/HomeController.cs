f3290741c8b122d83858e8f011a41e9a3d36818b
sushant jadhav <sushantjadhav416@gmail.com>
2024-01-20 13:23:00 +0530

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+using WebApplication2.Models;
+using WebApplication2.DTOs;
+
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
+
+        private readonly List<String> MostUsedLanguage = new List<String>()
+        {
+            "English",
+            "Mandarin Chinese",
+            "Hindi",
+            "Spanish",
+            "French",
+            "Modern Standard Arabic",
+            "Portuguese",
+            "Russian",
+            "Urdu",
+            "Indonesian",
+            "German",
+            "Japanese",
+            "Nigerian Pidgin",
+            "Marathi",
+            "Korean",
+            "Italian"
+        };
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration, HttpClient httpClient)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _httpClient = httpClient;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Languages = new SelectList(MostUsedLanguage);
+            return View();
+        }
+
+        public async Task<IActionResult> OpenAIGPT(string query,string selectedLanguage)
+        {
+            //Get openapi key from appsetting.json
+
+            var openAPIkey = _configuration["OpenAI:ApiKey"];
+
+            //Setup HttpClient with openAI Key
+            _httpClient.DefaultRequestHeaders.Add("Authorization",$"Bearer{openAPIkey}");
+
+            //Define the request Payload
+            var payload = new
+            {
+                model = "gpt-4",
+                message = new object[]
+                {
+                    new { role = "System", content = $"Translate to {selectedLanguage}" },
+                    new { role = "user", content = query }
+                },
+                temperature = 0,
+                max_tokens = 256
+
+            };
+
+            string jsonPayload = JsonConvert.SerializeObject(payload);
+
+            HttpContent httpcontent =  new StringContent(jsonPayload,Encoding.UTF8,"application/json");
+             
+            //Send the request
+            var responseMessage = await _httpClient.PostAsync("https://api.openai.com/v1/chat/completions",httpcontent);
+
+            var responseMessageJson = await responseMessage.Content.ReadAsStringAsync();
+
+            //Return Response
+            var Response = JsonConvert.DeserializeObject<OpenAIResponse>(responseMessageJson);
+
+            ViewBag.Result = Response.choices[0].Message.content;
+
+
+
+            return View();
+        }
+
+      
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication2/DTOs/OpenAIResponse.cs
@@ -0,0 +1,32 @@
+﻿namespace WebApplication2.DTOs
+{
+    public class OpenAIResponse
+    {
+
+        public string ID { get; set; }
+
+        public string Object { get; set; }
+
+        public int created { get; set; }
+
+        public string model { get; set; }
+
+
+        public List<Choices> choices {get;set ;}
+    }
+
+    public class Choices
+    {
+        public String Index { get; set; }
+        public Message Message { get; set; }
+
+        public string FinishedReason { get; set; }
+    }
+
+    public class Message
+    {
+        public String Role { get; set; }
+
+        public String content { get; set; }
+    }
+}
